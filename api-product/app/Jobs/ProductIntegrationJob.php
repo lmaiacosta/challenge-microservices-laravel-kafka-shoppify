7eec254e4ad8171d7bf9c8f660003f0e03ef12a7
Leonardo Maia Costa <lmaiacosta@gmail.com>
2024-01-28 04:27:10 -0300

    Update Jobs

--- api-product/app/Jobs/ProductIntegrationJob.php
+++ api-product/app/Jobs/ProductIntegrationJob.php
@@ -21,23 +21,9 @@ class ProductIntegrationJob implements ShouldQueue
 
     public function handle()
     {
-
-        Redis::connection()->lpush('integration_queue', json_encode([
+        Redis::connection()->rpush('integration_queue', json_encode([
             'action' => 'sync_product',
             'product' => $this->product->toArray(),
         ]));
-
-
-//        dispatch(new \App\Jobs\IntegrationQueueJob($this->product));
-//        dispatch(new IntegrationQueueJob($this->product));
-
-//        // Lógica para preparar dados e enviar para a fila no Microserviço de Integração
-//        $integrationData = [
-//            'action' => 'sync_product',
-//            'product' => $this->product->toArray(),
-//        ];
-//
-//        Redis::connection()->rpush('integration_queue', json_encode($this->product));
-////        dispatch(job: new IntegrationQueueJob($integrationData));
     }
 }
